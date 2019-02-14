4c9d10f6870dd6ee4dc21101d352f563bd6d91d7
Pattawan Kaewduangdee <pattawanna@gmail.com>
2019-02-14 21:22:39 +0100

    implement fetch data from db to provide get method and add post method

--- routes/payments.js
+++ routes/payments.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 /* GET payment listing. */
-router.get('/', (req, res) => {
-  res.send('Here is Get Payments');
+router.get('/', async (req, res) => {
+  try {
+    const results = await req.db.select().from('payments');
+    res.status(200).json({ results: results });
+  } catch (e) {
+    res.status(500).json({ message: 'Failed to query payments' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    await req.db.insert(req.body).into('payments');
+    res.status(200).send('Payment item added');
+  } catch (err) {
+    res.status(500).send('Payment item cannot be added');
+  }
 });
 
 module.exports = router;
--- tests/payments.test.js
+++ tests/payments.test.js
@@ -1,16 +1,40 @@
 const request = require('supertest');
 
-let server;
-
 describe('/payment', () => {
-  beforeEach(() => { server = require('../index'); });
-  afterEach(() => { server.close(); });
-
+  let server;
+  
+  beforeAll(() => {
+    server = require('../server');
+  });
+  
+  afterAll(() => {
+    server.close();
+  });
+  
   describe('GET /', () => {
-    it('Get proper response', async () => {
+    it('Get proper response', async (done) => {
       const result = await request(server).get('/payments');
       expect(result.status).toBe(200);
-      expect(result.text).toEqual('Here is Get Payments');
+      done();
     });
+  });
+
+  describe('POST /', () => {
+    it('Create payment successfully', async () => {
+      const date = new Date();
+      const data = {
+        contractId: 1,
+        description: 'Rent payment',
+        value: 1000,
+        time: date,
+        isImported: false,
+        createdAt: date,
+        updatedAt: date,
+        isDeleted: false
+      };
+      const result = await request(server).post('/payments').send(data);
+      expect(result.status).toBe(200);
+      expect(result.text).toEqual('Payment item added');
+    })
   });
 });
